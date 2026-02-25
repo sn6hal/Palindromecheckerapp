8196fd8fab6085aed2f7b5e400de8b67783aee30
Karthi-dev123 <karthikeyadevaraj@gmail.com>
2026-02-25 09:19:32 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    static void main() {
+        System.out.println("Welcome to the Palindrome Checker Management Systsem");
+        System.out.println("Version : 1.0 ");
+        System.out.println("System Initialized Successfully ");
+    }
+}
